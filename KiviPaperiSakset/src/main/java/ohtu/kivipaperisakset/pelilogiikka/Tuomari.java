745946c4717c4f7f549232bab5f23f5955075a64
emivo <emil.airta@helsinki.fi>
2016-05-07 13:59:01 +0300

    valmis 1 ja 2

--- KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/pelilogiikka/Tuomari.java
+++ KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/pelilogiikka/Tuomari.java
@@ -5,51 +5,61 @@ import java.util.Map;
 
 public class Tuomari {
 
-    private int ekanPisteet;
-    private int tokanPisteet;
-    private int tasapelit;
+    private final int ensimmaisenPelaajanPisteet;
+    private final int toisenPelaajanPisteet;
+
+    private final int tasapelit;
+    private final Map<Integer, Integer> tilanne;
     private final Map<String, String> voittoTaulu;
 
     public Tuomari() {
-        this.ekanPisteet = 0;
-        this.tokanPisteet = 0;
+        this.ensimmaisenPelaajanPisteet = 1;
+        this.toisenPelaajanPisteet = -1;
         this.tasapelit = 0;
         this.voittoTaulu = new HashMap<>();
-        alustaTaulu();
+        this.tilanne = new HashMap<>();
+        alustaTaulut();
     }
 
-    private void alustaTaulu() {
+    private void alustaTaulut() {
         this.voittoTaulu.put("k", "s");
         this.voittoTaulu.put("s", "p");
         this.voittoTaulu.put("p", "k");
+        this.tilanne.put(ensimmaisenPelaajanPisteet, 0);
+        this.tilanne.put(toisenPelaajanPisteet, 0);
+        this.tilanne.put(tasapelit, 0);
+    }
+    
+    public int getEnsimmaisenPelaajanPisteet() {
+        return tilanne.get(ensimmaisenPelaajanPisteet);
+    }
+
+    public int getToisenPelaajanPisteet() {
+        return tilanne.get(toisenPelaajanPisteet);
+    }
+
+    public int getTasapelit() {
+        return tilanne.get(tasapelit);
+    }
+    
+    private int voitto(String eka, String toka) {
+        if (voittoTaulu.get(eka).equals(toka)) return 1;
+        if (voittoTaulu.get(toka).equals(eka)) return -1;
+        return 0;
     }
 
     public void kirjaaSiirto(String ekanSiirto, String tokanSiirto) {
-        if (tasapeli(ekanSiirto, tokanSiirto)) {
-            tasapelit++;
-        } else if (ekaVoittaa(ekanSiirto, tokanSiirto)) {
-            ekanPisteet++;
-        } else {
-            tokanPisteet++;
-        }
+        int i = voitto(ekanSiirto, tokanSiirto);
+        tilanne.put(i, tilanne.get(i) + 1);
     }
-
-    private boolean tasapeli(String eka, String toka) {
-        return eka.equals(toka);
-    }
-
-    private boolean ekaVoittaa(String eka, String toka) {
-        return voittoTaulu.get(eka).equals(toka);
-    }
-
     @Override
     public String toString() {
         return "Pelitilanne: "
-                + ekanPisteet
+                + getEnsimmaisenPelaajanPisteet()
                 + " - "
-                + tokanPisteet
+                + getToisenPelaajanPisteet()
                 + "\nTasepelit: "
-                + tasapelit
+                + getTasapelit()
                 + "\n";
     }
 
